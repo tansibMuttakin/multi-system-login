d3ae7540010878a44622cbe738c31d3e81672da2
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-06-24 19:34:13 +0600

    re-setup foodpanda-app

--- foodpanda-app/vite.config.js
+++ foodpanda-app/vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
-        tailwindcss(),
+        react(),
     ],
+    server: {
+        port: 3001,
+    },
 });
